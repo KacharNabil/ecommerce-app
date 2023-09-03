85f37a3791f9eefc5bf54c8222be2d0663ae590b
Nabil Kachar <kacharnabil1@gmail.com>
2023-09-03 15:06:40 +0100

    fisrt push

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import Announcement from "./components/Announcement"
+import NavBar from "./components/NavBar"
+
+
+export default function App() {
+  return (
+    <>
+      <Announcement/>
+      <NavBar/>
+    </>
+  )
+}
---
+++ frontend/src/components/Announcement.jsx
@@ -0,0 +1,11 @@
+
+
+function Announcement() {
+  return (
+    <div className='h-[30px] bg-teal-600 p-0.5 text-white font-medium text-center'>
+        Free Shipping On Orders Over 30$
+    </div>
+  )
+}
+
+export default Announcement
