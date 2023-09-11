0fb86a3be42c56650f49a06ed2f8febee600e1d4
Nabil Kachar <kacharnabil1@gmail.com>
2023-09-11 14:20:47 +0100

    login and register are added

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -6,7 +6,7 @@ function Register ()  {
     <div className='bg-white w-[35%] p-8 rounded-xl shadow-xl '>
         <form action="" method="post"     >
           
-          <h1 className='font-bold text-[20px] text-center my-2'>Welcome Back To <span className='text-gray-300'>Snow</span>Shop</h1>
+          <h1 className='font-bold text-[20px] text-center my-2'>Welcome  To <span className='text-gray-300'>Snow</span>Shop</h1>
           <div  className='flex flex-wrap justify-center  '>
           <input type="text" placeholder='firstname' className='my-4 py-1.5 px-2 rounded-md font-medium bg-gray-100 border-2 border-gray-100 mr-5'/>
           <input type="text" placeholder='lastname' className='my-4 py-1.5 px-2 rounded-md font-medium bg-gray-100 border-2 border-gray-100 mr-5'/>
